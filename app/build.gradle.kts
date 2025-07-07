b67be874aaeda72d9a6f65907510ab33abb85f87
NTThanh2711 <thanhbct5abtx@gmail.com>
2025-07-07 13:15:53 +0700

    complete 50%

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,15 @@ android {
 
 dependencies {
 
+    implementation("com.tbuonomo:dotsindicator:4.2")
+    implementation(libs.simpleratingbar)
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/example/prm392_project/ui/activities/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.prm392_project.ui.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.example.prm392_project.R
+
+class MainActivity : AppCompatActivity() {
+
+}
